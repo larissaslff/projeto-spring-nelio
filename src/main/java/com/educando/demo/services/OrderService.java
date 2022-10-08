5f35c2777c47d4be6021d246f601d833da883170
larissaslff <110000521+larissaslff@users.noreply.github.com>
2022-10-08 16:00:51 -0300

    datas com instant

---
+++ src/main/java/com/educando/demo/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.educando.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educando.demo.entities.Order;
+import com.educando.demo.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> findAll(){
+        return repository.findAll();
+    }
+    
+    public Order findById(Long id) {
+        Optional<Order> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
