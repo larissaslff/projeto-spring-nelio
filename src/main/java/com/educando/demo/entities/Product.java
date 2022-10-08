023fa4e0d9faad370586a72b8a01dd77d464cb07
larissaslff <110000521+larissaslff@users.noreply.github.com>
2022-10-08 18:35:08 -0300

    Associação muitos-para-muitos

--- src/main/java/com/educando/demo/entities/Product.java
+++ src/main/java/com/educando/demo/entities/Product.java
@@ -4,10 +4,15 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.hibernate.annotations.ManyToAny;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
@@ -24,10 +29,11 @@ public class Product implements Serializable{
     private Double price;
     private String imgUrl;
 
-    @Transient
+    @ManyToMany
+    @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
     
-    Product() {
+    public Product() {
     }
 
     public Product(Long id, String name, String description, Double price, String imgUrl) {
