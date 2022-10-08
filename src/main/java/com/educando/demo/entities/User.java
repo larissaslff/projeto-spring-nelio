b4dc344bf729bd47d56828e2e930649607e1349c
larissaslff <110000521+larissaslff@users.noreply.github.com>
2022-10-08 12:35:21 -0300

    Entidade pedido

--- src/main/java/com/educando/demo/entities/User.java
+++ src/main/java/com/educando/demo/entities/User.java
@@ -1,11 +1,14 @@
 package com.educando.demo.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -13,6 +16,7 @@ import jakarta.persistence.Table;
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
     
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -20,6 +24,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User (){
 
@@ -71,6 +78,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
